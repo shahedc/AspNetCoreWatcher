270a11c1c6207f3f89092c64ed8dc87e9fc77e53
shahedc <shahedc@gmail.com>
2018-11-25 19:44:39 -0500

    initial code with CalcController sample code
    initial code with CalcController sample code

---
+++ Controllers/CalcController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreWatcher.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalcController : ControllerBase
+    { 
+        [Route("[action]")]
+        public ActionResult<string> Sum(
+            [FromQuery(Name = "num1")] int num1,
+            [FromQuery(Name = "num2")] int num2)
+        {
+            var sum = (num1 + num2).ToString();
+            return $"The sum of numbers {num1} and {num2} is {sum}.";
+        }
+
+        [Route("[action]")]
+        public ActionResult<string> SumInts(
+            [FromQuery(Name = "num1")] int num1,
+            [FromQuery(Name = "num2")] int num2)
+        {
+            var sum = (num1 + num2).ToString();
+            return $"The sum of ints {num1} and {num2} is {sum}.";
+        }
+
+        [Route("[action]")]
+        public ActionResult<string> SumDoubles(
+            [FromQuery(Name = "num1")] double num1,
+            [FromQuery(Name = "num2")] double num2)
+        {
+            var sum = (num1 + num2).ToString();
+            return $"The sum of doubles {num1} and {num2} is {sum}.";
+        }
+    }
+}
